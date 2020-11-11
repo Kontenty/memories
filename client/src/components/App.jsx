d74803a28b015dab32a55ebd2ad14b3bb04dbc1c
Konrad Gorski <konradgrsk@gmail.com>
2020-11-11 20:04:01 +0100

    base setup and layout with chakra ui

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Box, Flex, Heading, Image, theme } from "@chakra-ui/core";
+
+import memoImg from "../assets/images/memories.jpg";
 
 const App = () => {
+  console.log(theme);
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <Flex
+        as="nav"
+        h="60px"
+        bg="pink.700"
+        color="white"
+        px={["20px", "20vw"]}
+        alignItems="center"
+      >
+        <Heading>Memories</Heading>
+        <Box w="60px">
+          <Image src={memoImg} alt="memories" />
+        </Box>
+      </Flex>
+      <Box maxW="1200px" px="40px" mx="auto">
+        <h1>App</h1>
+      </Box>
+    </>
   );
 };
 
