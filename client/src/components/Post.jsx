1da62f2017056f70600ca0fa17dfa122a9128d81
Konrad Gorski <konradgrsk@gmail.com>
2020-11-15 18:31:45 +0100

    recoil fetch in posts not in post

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-
-import { posts } from "../atoms";
 
 const Post = () => {
-  const postsData = useRecoilValue(posts);
-  console.log(postsData);
   return (
     <div>
       <h3>Post</h3>
--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+
+import { posts } from "../atoms";
 
 import Post from "./Post";
 
 const Posts = () => {
+  const postsData = useRecoilValue(posts);
+  console.log(postsData);
   return (
     <div>
       <Post />
