3b4caffd52d1fdfe71031203916d766a69eecf4c
Konrad Gorski <konradgrsk@gmail.com>
2020-11-29 14:12:28 +0100

    server delete route

--- src/server/controllers/postsCtrl.js
+++ src/server/controllers/postsCtrl.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+import log from "loglevel";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -22,6 +25,27 @@ export const createPost = async (req, res) => {
   try {
     await newPost.save();
     res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const {
+    body: { id },
+  } = req;
+
+  try {
+    const post = await PostMessage.findById(id);
+    const { selectedFile } = post;
+    const image = selectedFile.match(/\/[a-z0-9_-]*.(jpg|png)$/g);
+    await PostMessage.deleteOne(post);
+    try {
+      fs.unlinkSync(path.join(process.cwd(), "/uploads", image[0]));
+    } catch (error) {
+      log.error(error);
+    }
+    res.status(200).json({ message: " post deleted", post });
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
--- src/server/routes/post.route.js
+++ src/server/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { createPost, getPosts } from "../controllers/postsCtrl.js";
+import { createPost, deletePost, getPosts } from "../controllers/postsCtrl.js";
 import imageResize from "../controllers/imgResizeCtrl.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", upload.single("file"), imageResize, createPost);
+router.delete("/", deletePost);
 
 export default router;
